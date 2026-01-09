49fd42d17e983432fc19a35b51220b78d60d4817
Marc Kurz <marc.kurz@fh-ooe.at>
2026-01-09 11:43:40 +0100

    modified setup script in rerere

--- 05_rerere/setup.sh
+++ 05_rerere/setup.sh
@@ -7,7 +7,8 @@ rm -rf "$DEMO_DIR"
 mkdir "$DEMO_DIR"
 cd "$DEMO_DIR"
 
-git init -q
+# Stelle sicher, dass der Default-Branch main ist
+git init -q -b main 2>/dev/null || git init -q
 
 cat <<EOF > story.txt
 Once upon a time, there was a shared file.
@@ -17,13 +18,31 @@ EOF
 git add story.txt
 git commit -qm "Initial story"
 
+# Falls git init -b main nicht unterstützt wurde: Branch nachträglich auf main setzen
+git branch -M main >/dev/null 2>&1 || true
+
+# Portable "in-place replace"
+replace_in_file () {
+  local search="$1"
+  local repl="$2"
+  local file="$3"
+
+  if sed --version >/dev/null 2>&1; then
+    # GNU sed
+    sed -i "s/${search}/${repl}/" "$file"
+  else
+    # BSD/macOS sed
+    sed -i '' "s/${search}/${repl}/" "$file"
+  fi
+}
+
 git checkout -b feature-a -q
-sed -i 's/Line that both branches will modify./Line changed by feature A./' story.txt || perl -pi -e 's/Line that both branches will modify./Line changed by feature A./' story.txt
+replace_in_file "Line that both branches will modify\." "Line changed by feature A." story.txt
 git commit -am "Change line in feature A" -q
 
 git checkout main -q
 git checkout -b feature-b -q
-sed -i 's/Line that both branches will modify./Line changed by feature B./' story.txt || perl -pi -e 's/Line that both branches will modify./Line changed by feature B./' story.txt
+replace_in_file "Line that both branches will modify\." "Line changed by feature B." story.txt
 git commit -am "Change line in feature B" -q
 
 git checkout feature-a -q
